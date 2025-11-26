7942897511db560c0fd4e8c71fcbcfaeca79e110
Adrián Mercado <adrianmercado782@gmail.com>
2025-11-26 12:42:43 -0400

    dtos de rutina

---
+++ Models/DTOS/CreateRutinaDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Proyecto.DTOs.Rutina
+{
+    public class CreateRutinaDto
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Objetivo { get; set; }
+        public string NivelObjetivo { get; set; }
+
+        public List<int> EjerciciosIds { get; set; } = new();
+    }
+}
