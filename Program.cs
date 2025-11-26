b791c89dfca14625a96cb71055e84c0284896d28
Adrián Mercado <adrianmercado782@gmail.com>
2025-11-26 19:40:12 -0400

    fix

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Npgsql;
+using Secuity.Repositories;
 using Security.Data;
 using Security.Repositories;
 using Security.Services;
@@ -100,6 +101,8 @@ builder.Services.AddScoped<IHospitalRepository, HospitalRepository>();
 builder.Services.AddScoped<IHospitalService, HospitalService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IRutinaRepository, RutinaRepository>();
+builder.Services.AddScoped<IRutinaService, RutinaService>();
 
 var app = builder.Build();
 
