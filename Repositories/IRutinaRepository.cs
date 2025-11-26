3f300f704732d6a67d5e96ce9de2c3e972d35f32
Adrián Mercado <adrianmercado782@gmail.com>
2025-11-26 12:45:42 -0400

    Rutina repository added with its interface

---
+++ Repositories/IRutinaRepository.cs
@@ -0,0 +1,13 @@
+﻿using Proyecto.Entities;
+
+namespace Proyecto.Repositories
+{
+    public interface IRutinaRepository
+    {
+        Task<Rutina> CreateAsync(Rutina rutina);
+        Task<Rutina> GetByIdAsync(int id);
+        Task<List<Rutina>> GetAllAsync();
+        Task UpdateAsync(Rutina rutina);
+        Task DeleteAsync(Rutina rutina);
+    }
+}
---
+++ Repositories/RutinaRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto.Data;
+using Proyecto.Entities;
+
+namespace Proyecto.Repositories
+{
+    public class RutinaRepository : IRutinaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public RutinaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Rutina> CreateAsync(Rutina rutina)
+        {
+            _context.Rutinas.Add(rutina);
+            await _context.SaveChangesAsync();
+            return rutina;
+        }
+
+        public async Task<Rutina> GetByIdAsync(int id)
+        {
+            return await _context.Rutinas
+                .Include(r => r.Ejercicios)
+                .Include(r => r.Usuarios)
+                .Include(r => r.Creador)
+                .FirstOrDefaultAsync(r => r.Id == id);
+        }
+
+        public async Task<List<Rutina>> GetAllAsync()
+        {
+            return await _context.Rutinas
+                .Include(r => r.Ejercicios)
+                .Include(r => r.Creador)
+                .ToListAsync();
+        }
+
+        public async Task UpdateAsync(Rutina rutina)
+        {
+            _context.Rutinas.Update(rutina);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Rutina rutina)
+        {
+            _context.Rutinas.Remove(rutina);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
